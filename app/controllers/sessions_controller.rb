77ba8b2bac61489832e6841c9e0b7411e01bb382
jrubyuncle <jrubyuncle@gmail.com>
2015-10-14 22:44:39 +0800

    add sessions#create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    n = params[:name]
+    p = params[:password]
+
+    if u = User.find_by(name: n).try(:authenticate, p)
+      session[:user_id] = u.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   get 'sessions/new'
 
   resources :posts
