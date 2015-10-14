4f16e09c5d8ef70ff1891ae56b4538d5a3f40470
jrubyuncle <jrubyuncle@gmail.com>
2015-10-14 22:27:11 +0800

    add user_params in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
   def to_s
     name
   end
+
+  protected
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
