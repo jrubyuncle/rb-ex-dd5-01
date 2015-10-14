f5b73a3d76c048128009c4b96ad24b032826aa26
jrubyuncle <jrubyuncle@gmail.com>
2015-10-14 21:51:22 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014134713) do
+ActiveRecord::Schema.define(version: 20151014134907) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20151014134713) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "posts_tags", id: false, force: :cascade do |t|
+    t.integer "tag_id",  null: false
+    t.integer "post_id", null: false
+  end
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
