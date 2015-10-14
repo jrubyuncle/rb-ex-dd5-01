06d7b909b5bb5893331a86401c93ac28b453d637
jrubyuncle <jrubyuncle@gmail.com>
2015-10-14 22:53:18 +0800

    add helper for application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :user_signed_in?
+
+
+  protected
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def user_signed_in?
+    current_user
+  end
 end
